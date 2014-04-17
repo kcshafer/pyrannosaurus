88d9a51a617ea92aa6b5d4fae28a2f0bc54f3a21
KC Shafer <kshafer@2u.com>
2014-04-17 12:54:03 +0000

    Finishing run tests support

--- pyrannosaurus/clients/apex.py
+++ pyrannosaurus/clients/apex.py
@@ -6,8 +6,10 @@ from suds.client import Client
 from suds.cache import FileCache
 
 from pyrannosaurus.clients.base import BaseClient
-
+    
 class ApexClient(BaseClient):
+    _debug_levels = ['Db', 'Workflow', 'Validation', 'Callout', 'Apex_code', 'Apex_profiling']
+    
     def __init__(self, wsdl='wsdl/apex.xml', cacheDuration=0, **kwargs):
         super(ApexClient, self).__init__()
         #TODO: clean this up
@@ -26,6 +28,29 @@ class ApexClient(BaseClient):
         headers = {'User-Agent': 'Salesforce/' + self._product + '/' + '.'.join(str(x) for x in self._version)}
         self._client.set_options(headers = headers)
 
+    #TODO : this really won't work, needs check for sobjecttype to go to right client
+    def generateHeader(self, sObjectType):
+        try:
+          return self._client.factory.create(sObjectType)
+        except:
+          print 'There is not a SOAP header of type %s' % sObjectType
+    
+    def _default_log_info(self):
+        log_info = self._client.factory.create('LogInfo')
+        log_info.category = 'All'
+        log_info.level = 'Debug'
+        return log_info
+
+    #TODO eval
+    def _setHeaders(self, call = None):
+        headers = {'SessionHeader': self._sessionHeader}
+        print self._sessionHeader
+        if(call == 'runTests'):
+            debug_header = self.generateHeader('DebuggingHeader')
+            print debug_header
+            debug_header.categories.append(self._default_log_info())  
+        self._client.set_options(soapheaders = headers)
+
     def login(self, username, password, token=''):
         lr = super(ApexClient, self)._login(username, password, token)
         self._setEndpoint("https://cs18.salesforce.com/services/Soap/s/29.0")
@@ -42,7 +67,8 @@ class ApexClient(BaseClient):
         run_tests_request.allTests = all_tests
         run_tests_request.classes = classes
         run_tests_request.namespace = namespace
+        run_tests_request.packages = None
 
-        run_tests_result = self._client.service.runTests(run_tests_request)
+        run_tests_response = self._client.service.runTests(run_tests_request)
 
-        return run_tests_result
+        return run_tests_response
