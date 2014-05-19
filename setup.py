9bf789d9f0cf9a943ac6704d9180df690c63d3e6
KC Shafer <kshafer@2u.com>
2014-05-19 15:11:21 +0000

    Correctly requiring lxml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name = "pyrannosaurus",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "Salesforce Development Tools",
     author = "KC Shafer",
     author_email = "kclshafer@gmail.com",
@@ -11,8 +11,8 @@ setup(
     keywords = ["salesforce"],
     install_requires = [
         "suds==0.4",
-        "lxml=3.3.4",
-    ],
+        "lxml==3.3.4",
+        ],
     package_dir={},
     include_package_data=True,
     packages=['pyrannosaurus', 'wsdl'],
