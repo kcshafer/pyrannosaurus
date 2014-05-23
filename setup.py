9968b5298a9f30535a6acf470a743066b4f6b6c4
KC Shafer <kshafer@2u.com>
2014-05-23 15:52:34 +0000

    Bumping version to 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name = "pyrannosaurus",
-    version = "0.0.4",
+    version = "0.0.5",
     description = "Salesforce Development Tools",
     author = "KC Shafer",
     author_email = "kclshafer@gmail.com",
